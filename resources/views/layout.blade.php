014487240aedf4ff7228ad87d7b847b5b2e83085
johana <elvia.rodriguez@goes.gob.sv>
2024-03-08 20:21:37 -0600

    ajustes del home

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -6,12 +6,14 @@
     <title>@yield('title', 'OASYS')</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+</head>
     <link rel="stylesheet" href="estilos.css">
 </head>
 <body>
     <!-- Header -->
     <header class="header"><!--agregar fixed top para fijar el menu-->
-        <img class="imagenLogo" src="imagenes/logo.png" style="max-height: 90px;">
+        <img class="imagenLogo" src="imagenes/BLANCO.png" style="max-height: 90px;">
         <nav>
             <ul>
                 <li><a href="#">Inicio</a></li>
@@ -29,12 +31,33 @@
     <!-- Footer -->
     <footer class="bg-dark text-light text-center py-4">
         <div class="container">
-            <p>© {{ date('Y') }} OASYS. Todos los derechos reservados.</p>
+            <div class="row align-items-center  pt-3 pb-3">
+                <div class="col-md-4">
+                    <img class="logo-footer" src="imagenes/logo.png" style="max-height: 110px;">
+                </div>
+               
+                <div class="col-md-4">
+                    <h5>Contáctanos:</h5>
+                    <div class="pl-2 d-flex"><i class="fa fa-phone" aria-hidden="true"></i>
+                        <p class="pl-2 mb-0">+503(6858-5513)</p>
+                    </div>
+                    <div class="pl-2 d-flex"> <i class="fa fa-mouse-pointer" aria-hidden="true"></i>
+                        <p class="pl-2 mb-0">www.oasys.com</p>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <p>© {{ date('Y') }} OASYS. Todos los derechos reservados.</p>
+                </div>
+            </div>
         </div>
     </footer>
 
     <!-- Scripts -->
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </body>
 </html>
---
+++ resources/views/nosotros.blade.php
@@ -0,0 +1,10 @@
+@extends('layout')
+
+@section('title', 'Inicio')
+
+@section('content')
+<div class="container">
+    <h1>pagina nosotros</h1>
+</div>
+
+@endsection
